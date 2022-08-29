47ab5f7dfa893a95662a09ee00ac79a837e60947
Christopher Butler <cabutler@mit.edu>
2022-08-29 07:51:44 -0400

    Rename Matomo ECR
    Why these changes are being introduced:
    Per a conversation in the #engineering channel in Slack, we have
    decided that all of our repos that are really just modifications of
    a third party Docker container should have "docker-" as the prefix
    of the name. Since there is a tight relationship between the app
    GitHub repo name, the ECR repo name, and the OIDC roles/policies for
    automated deployments, the ECR configuration needs to be updated.
    
    How this addresses that need:
    * Change the app-repo name from "matomo" to "docker-matomo" in the
    module call that creates the ECR and related bits
    
    Side effects of this change:
    This will delete the current ECR, roles, policies, and outputs and
    recreate them. The GitHub repo for Matomo will also need to be renamed
    and the new Makefile and GHA workflow files will need to be updated.
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IN-487

--- matomo_ecr.tf
+++ matomo_ecr.tf
@@ -6,13 +6,13 @@ locals {
 }
 module "ecr_matomo" {
   source            = "./modules/ecr"
-  repo_name         = "matomo"
+  repo_name         = "docker-matomo"
   login_policy_arn  = aws_iam_policy.login.arn
   oidc_arn          = data.aws_ssm_parameter.oidc_arn.value
   environment       = var.environment
   tfoutput_ssm_path = var.tfoutput_ssm_path
   tags = {
-    app-repo = "matomo"
+    app-repo = "docker-matomo"
   }
 }
 
