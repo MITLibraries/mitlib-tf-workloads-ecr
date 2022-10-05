7e17235db673493430c963b2ed059c04ea10d4fe
Christopher Butler <cabutler@mit.edu>
2022-10-05 16:50:38 -0400

    Create ECR for Carbon and Other Updates
    Why these changes are being introduced:
    The Carbon app introduces a few changes to our automated build process.
    There are changes in the shared workflows in the MIT Libraries .giuthub
    repository that reflect the automated build changes necessary for
    carbon. The changes in this repo update the Terraform Outputs to match
    the new input requirements of the shared workflows.
    
    As part of addressing the needs of the Carbon app, it made sense to
    consolidate the shared workflows by using a few conditional steps. The
    changes here continue that idea of consolidation with conditional text.
    
    How this addresses that need:
    * Create carbon_ecr.tf to buid the ECR repository for the Carbon app
    * Create three new template files for dev-build, stage-build, and
    prod-promote caller workflow outputs
    * Create one new template file for the Makefile output
    * Update the output resources for just the carbon ECR to use the
    new template files
    
    Side effects of this change:
    None. In fact, the only changes are changes to output values. This will
    not make any changes to the infrastructure in AWS.
    
    In the near future, we will need to update the Terraform Outputs to
    use the new templates and eventually remove the outdated templates
    once we have upgraded across our multiple repositories.

---
+++ carbon_ecr.tf
@@ -0,0 +1,68 @@
+
+# carbon containers
+# This is a standard ECR for an ECS with a Fargate launch type
+locals {
+  ecr_carbon = "carbon-${var.environment}"
+}
+module "ecr_carbon" {
+  source            = "./modules/ecr"
+  repo_name         = "carbon"
+  login_policy_arn  = aws_iam_policy.login.arn
+  oidc_arn          = data.aws_ssm_parameter.oidc_arn.value
+  environment       = var.environment
+  tfoutput_ssm_path = var.tfoutput_ssm_path
+  tags = {
+    app-repo = "carbon"
+  }
+}
+
+
+## Outputs to Terraform Cloud for devs ##
+
+## For carbon application repo and ECR repository
+# Outputs in dev
+output "carbon_dev_build_workflow" {
+  value = var.environment == "prod" || var.environment == "stage" ? null : templatefile("${path.module}/files/dev-build.tpl", {
+    region   = var.aws_region
+    role     = module.ecr_carbon.gha_role
+    ecr      = module.ecr_carbon.repository_name
+    function = ""
+    }
+  )
+  description = "Full contents of the dev-build.yml for the carbon repo"
+}
+output "carbon_makefile" {
+  value = var.environment == "prod" || var.environment == "stage" ? null : templatefile("${path.module}/files/makefile.tpl", {
+    ecr_name = module.ecr_carbon.repository_name
+    ecr_url  = module.ecr_carbon.repository_url
+    function = ""
+    }
+  )
+  description = "Full contents of the Makefile for the carbon repo (allows devs to push to Dev account only)"
+}
+
+# Outputs in stage
+output "carbon_stage_build_workflow" {
+  value = var.environment == "prod" || var.environment == "dev" ? null : templatefile("${path.module}/files/stage-build.tpl", {
+    region   = var.aws_region
+    role     = module.ecr_carbon.gha_role
+    ecr      = module.ecr_carbon.repository_name
+    function = ""
+    }
+  )
+  description = "Full contents of the stage-build.yml for the carbon repo"
+}
+
+# Outputs after promotion to prod
+output "carbon_prod_promote_workflow" {
+  value = var.environment == "stage" || var.environment == "dev" ? null : templatefile("${path.module}/files/prod-promote.tpl", {
+    region     = var.aws_region
+    role_stage = "${module.ecr_carbon.repo_name}-gha-stage"
+    role_prod  = "${module.ecr_carbon.repo_name}-gha-prod"
+    ecr_stage  = "${module.ecr_carbon.repo_name}-stage"
+    ecr_prod   = "${module.ecr_carbon.repo_name}-prod"
+    function   = ""
+    }
+  )
+  description = "Full contents of the prod-promote.yml for the carbon repo"
+}
